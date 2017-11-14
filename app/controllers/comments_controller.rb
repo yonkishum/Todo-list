52b15fb916557cb373cc420a795f745f41247974
Yan <Yonkishum@gmail.com>
2017-11-13 20:09:29 -0400

    working on the new views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,4 +7,18 @@ class CommentsController < ApplicationController
       format.json
     end
   end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(description: params[:description][:description])
+    @comment.save
+    redirect_to @comment
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :todos
-
-  get '/comment' => 'comments#index'
+  resources :comments
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
